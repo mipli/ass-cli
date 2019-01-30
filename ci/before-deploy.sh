4b38dc041aa126df94a1eb66f7079fc68e2db30a
Michael A. Plikk <michael@aptoma.com>
2019-01-30 21:10:37 +0100

    Added Travis build

---
+++ ci/before-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cargo_out_dir() {
+  # This works by finding the most recent stamp file
+  target_dir="$1"
+  find "$target_dir" -name ass-cli-stamp -print0 \
+    | xargs -0 ls -t \
+    | head -n1 \
+    | xargs dirname
+}
+
+main() {
+  cargo build --target "$TARGET" --release
+
+  local tmpdir="$(mktemp -d)"
+  local name="ass-cli-${TARGET}"
+  local staging="$tmpdir/$name"
+  local out_dir="$(pwd)/deployment"
+  mkdir -p "$out_dir"
+
+  local cargo_out_dir="$(cargo_out_dir "target/$TARGET")"
+
+  cp "target/$TARGET/release/ass-cli" "$staging/ass-cli"
+  "strip" "$staging/ass-cli"
+
+  cp {README.md,LICENSE-MIT} "$staging/"
+
+  (cd "$tmpdir" && tar czf "$out_dir/$name.tar.gz" "$name")
+  rm -rf "$tmpdir"
+}
+
+main
